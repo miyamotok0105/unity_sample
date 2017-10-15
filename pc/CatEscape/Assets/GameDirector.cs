01ae8378e9de9d47e5b2d461263263f3e68faf91
Keiichiro Miyamoto <miyamotok0105@gmail.com>
2017-10-16 08:17:51 +0900

    add

---
+++ pc/CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UIを使うので忘れずに追加
+
+public class GameDirector : MonoBehaviour {
+
+	GameObject hpGauge;
+
+	void Start() {
+		//GameObject.Find：シーン内のオブジェクトを取得する関数
+		//注意：対象がアクティブになっていないときはnullになる
+		this.hpGauge = GameObject.Find("hpGauge");
+	}
+
+	public void DecreaseHp() {
+		//GetComponent；コンポーネントにアクセス
+		//fillAmount：どのくらいImageを描画するかを0~1の範囲で設定
+		this.hpGauge.GetComponent<Image> ().fillAmount -= 0.1f;
+	}
+}
